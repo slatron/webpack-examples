b0e4d58f223259a58e689d44dab4adae0392f88a
Slatron <slatron@gmail.com>
2020-10-20 14:24:21 -0500

    updating message and navigation templates

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,30 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const path = require("path")
 
+// Helper methods and consts to build page individual page configs
+// - should be in separate module post-POC
+const msg = `
+<p>
+  This is the MPA model that would be just like the current gulp anavo-client build. Each page of the application calls the same javascript bundle.
+</p>
+
+<p>
+  Compared to the <a href="https://github.com/slatron/webpack-examples/tree/mpa-agnostic-multiple-bundles" target="_blank">multiple bundle example</a>, this uses a data-load attribute on each html page button. Clicking the button calls the same method to call the API with a different parameter.
+</p>
+
+<p>
+  If we go with a MPA approach, we should use this model to keep the number of bundles at one. If we need greater isolation between pages, we can try a multiple bundle approach at that time.
+</p>
+`
+
+const nav = `
+<nav>
+  <ul>
+    <li><a href="/photos.html">photo page</a></li>
+    <li><a href="/todos.html">todo page</a></li>
+  </ul>
+</nav>
+`
 const generateHtmlPlugin = (title) => {
   return new HtmlWebpackPlugin({
     title,
@@ -10,11 +34,11 @@ const generateHtmlPlugin = (title) => {
 }
 
 const populateHtmlPlugins = (pagesArray) => {
-  res = [];
+  let plugins = []
   pagesArray.forEach(page => {
-    res.push(generateHtmlPlugin(page));
+    plugins.push(generateHtmlPlugin(page))
   })
-  return res;
+  return plugins
 }
 
 const pages = populateHtmlPlugins([
